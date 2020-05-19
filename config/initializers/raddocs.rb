4ec500a79e19e2b28911e4b00e0d6f97862a9acd
W Millon <wjmillon@gmail.com>
2020-05-19 08:50:45 -0500

    Dev Setup complete
    added necessary libs and config updates to support development

---
+++ config/initializers/raddocs.rb
@@ -0,0 +1,5 @@
+Raddocs.configure do |config|
+  config.docs_dir = 'doc/api'
+  config.api_name = 'User Management'
+  config.include_bootstrap = true
+end
---
+++ spec/acceptance/user_acceptance_spec.rb
@@ -0,0 +1,48 @@
+require 'v1_acceptance_helper'
+
+resource 'Users' do
+
+  explanation <<-EX
+  Users are managed through one route that
+  accepts different types of requests
+  EX
+
+  header 'Accept', 'application/json'
+  header 'Content-Type', 'application/json'
+
+  post '/api/v1/users' do
+    with_options scope: :data, with_example: true do
+      parameter :email, 'Unique user identifier', required: true
+      parameter :password, '************', required: true
+      parameter :roles, 'Grants users access to protected data', required: true
+      parameter :username, 'Custome identifier created by the user'
+    end
+
+    context 'queries' do
+
+      example 'for single user' do
+      end
+
+      example 'for all users valid role' do
+      end
+
+      example 'for all users invalid role' do
+      end
+    end
+
+    context 'mutations' do
+
+      example 'for user creation' do
+      end
+
+      example 'for user password change' do
+      end
+
+      example 'for user info updates' do
+      end
+
+      example 'for user removal' do
+      end
+    end
+  end
+end
